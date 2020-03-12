8c860acae4c0b875a78684b04c66f609ef140ef6
rmn976 <rmn976dev@gmail.com>
2020-03-12 17:38:31 +0100

    Fetch de base fonctionnel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,19 @@
 <script>
 
   export default {
-    name: 'Home'
+    name: 'Home',
+    mounted: function () {
+      this.id = this.$route.params.id
+      fetch('https://api.themoviedb.org/3/search/movie?api_key=3343ddbfc8f76de0f8b2c6901a410684&language=en-US&query='star'&page=1&include_adult=false')
+              .then(result => result.json())
+              .then((json) => {
+                this.film = json;
+                console.log(json);
+              })
+              .catch((error) => {
+                console.error(`Une erreur s'est prduite.`);
+                console.log(error);
+              });
+    },
   }
 </script>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>Film</div>
+    <div>Film {{ film.original_title }}</div>
 </template>
 
 <script>
     export default {
-        name: "Movie"
+        name: "Movie",
+        data: function () {
+            return {
+                film: null,
+                id: 0
+            }
+        },
+        mounted: function () {
+            this.id = this.$route.params.id
+            fetch('https://api.themoviedb.org/3/movie/' + this.id + '?api_key=3343ddbfc8f76de0f8b2c6901a410684&language=en-US')
+                .then(result => result.json())
+                .then((json) => {
+                    this.film = json;
+                    console.log(json);
+                })
+                .catch((error) => {
+                    console.error(`Une erreur s'est prduite.`);
+                    console.log(error);
+                });
+        },
     }
 </script>
 
