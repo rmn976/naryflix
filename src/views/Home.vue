106f133971f8c822fa65eeec152f0f68208671cd
rmn976 <rmn976dev@gmail.com>
2020-03-29 23:17:47 +0200

    Devoir rendu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,17 @@
     <div class="row m-0">
       <div class="offset-2 col-8">
         <MovieList v-if="search !== ''"></MovieList>
-        <carousel v-else :perPage="4" :mouseDrag="false" :autoplay="true" :autoplayTimeout="2000" :speed="1500" :loop="true">
-          <slide v-for="movie in popularList" :key="movie.id">
-            <router-link :to="{ name: 'Movie', params: { id: movie.id }}">
-              <img @click="setMovie(movie)" v-if="movie.poster_path != null" :src="'https://image.tmdb.org/t/p/w200' + movie.poster_path">
-              <img @click="setMovie(movie)" v-else src="../assets/image-non-disponible.png">
-            </router-link>
-          </slide>
-        </carousel>
+        <div v-else>
+          <h2 class="text-white text-center une">A la Une sur <img src="https://fontmeme.com/permalink/200328/4647fa15c30f81ce7987cb8d6c7ddc02.png"></h2>
+          <carousel class="mt-4" :perPage="4" :mouseDrag="false" :autoplay="true" :autoplayTimeout="2000" :speed="1500" :loop="true">
+            <slide v-for="movie in popularList" :key="movie.id">
+              <router-link :to="{ name: 'Movie', params: { id: movie.id }}">
+                <img @click="setMovie(movie)" v-if="movie.poster_path != null" :src="'https://image.tmdb.org/t/p/w200' + movie.poster_path">
+                <img @click="setMovie(movie)" v-else src="../assets/image-non-disponible.png">
+              </router-link>
+            </slide>
+          </carousel>
+        </div>
       </div>
     </div>
   </div>
@@ -49,7 +52,7 @@
 </script>
 
 <style scoped>
-  .VueCarousel {
-    margin-top: 20vh!important;
+  .une {
+    margin-top: 15vh!important;
   }
 </style>
